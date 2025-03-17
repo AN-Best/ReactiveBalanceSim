9b8e886827d3f6abf008929af1a353c097167ac3
AN-Best <anbest.37.7@gmail.com>
2025-03-17 15:13:09 -0700

    Standing code update - working (no sled)

--- standing.py
+++ standing.py
@@ -1,13 +1,12 @@
 import derive
 from derive import time_symbol, contact_force
 import numpy as np
-from scipy.integrate import odeint
 from opty import Problem
-from opty.utils import parse_free, f_minus_ma
+from opty.utils import f_minus_ma
 from symmeplot.matplotlib import Scene3D
 import matplotlib.pyplot as plt
 import sympy as sm
-
+import os
 #Derive the equations of motion
 (mass_matrix, forcing_vector, kane, constants, coordinates, speeds, specified,
  visualization_frames, ground, origin, segments) = \
@@ -21,7 +20,7 @@ par_map = derive.load_constants(constants,'example_constants.yml')
 
 #Discretization characteristics
 speed = 0.0 # m/s
-num_nodes = 40
+num_nodes = 50
 h = sm.symbols('h', real=True, positive=True)
 duration = (num_nodes - 1)*h
 
@@ -33,16 +32,16 @@ states = coordinates + speeds + [delt]
 num_states = len(states)
 
 #Pull out the coordinates and speeds
-#ax = lumbar position x
-#ay = lumbar position y
-#a = absolute torso angle
-#b = lumbar angle
-#c = right hip angle
-#d = right knee angle
-#e = right ankle angle
-#f = left hip angle
-#g = left knee angle
-#h = left ankle angle
+#ax = lumbar x
+#ay = lumbar y
+#a = absolute torso rotation
+#b = lumbar 
+#c = right hip 
+#d = right knee 
+#e = right ankle 
+#f = left hip 
+#g = left knee 
+#h = left ankle 
 qax, qay, qa, qb, qc, qd, qe, qf, qg, qh = coordinates
 uax, uay, ua, ub, uc, ud, ue, uf, ug, uh = speeds
 Fax, Fay, Ta, Tb, Tc, Td, Te, Tf, Tg, Th = specified
@@ -54,35 +53,36 @@ traj_map = {Fax: np.zeros(num_nodes),
 
 #Add Bounds
 bounds = {
-    h: (0.001, 0.1),
+    h: (0.1, 0.1),
     delt: (0.0, 10.0),
-    qax: (0.0, 10.0),
+    qax: (0.0, 0.0),
     qay: (0.5, 2.0),
-    qa: np.deg2rad((-60.0, 60.0)),
-    uax: (0.0, 10.0),
-    uay: (-10.0, 10.0),
+    qa: np.deg2rad((0.0, 0.0)),
+    uax: (-0.1, 0.1),
+    uay: (-0.1, 0.1),
 }
 
 #lumbar
-bounds.update({k: (-np.deg2rad(30.0), np.deg2rad(30.0))
+bounds.update({k: (-np.deg2rad(1.0), np.deg2rad(1.0))
                for k in [qb]})
 # hip
-bounds.update({k: (-np.deg2rad(40.0), np.deg2rad(40.0))
+bounds.update({k: (-np.deg2rad(1.0), np.deg2rad(1.0))
                for k in [qc, qf]})
 # knee
-bounds.update({k: (-np.deg2rad(60.0), 0.0)
+bounds.update({k: (-np.deg2rad(1.0), 0.0)
                for k in [qd, qg]})
-# foot
-bounds.update({k: (-np.deg2rad(30.0), np.deg2rad(30.0))
+# ankle
+bounds.update({k: (-np.deg2rad(1.0), np.deg2rad(1.0))
                for k in [qe, qh]})
 # all rotational speeds
-bounds.update({k: (-np.deg2rad(400.0), np.deg2rad(400.0))
+bounds.update({k: (-np.deg2rad(1.0), np.deg2rad(1.0))
                for k in [ua, ub, uc, ud, ue, uf, ug, uh]})
 # all joint torques
 bounds.update({k: (-100.0, 100.0)
                for k in [Tb, Tc, Td, Te, Tf, Tg, Th]})
 
 #Instance Constraints
+
 instance_constraints = (
     delt.func(0*h) - 0.0,
     qax.func(0*h) - 0.0,
@@ -107,6 +107,7 @@ instance_constraints = (
     ug.func(0*h) - ud.func(duration),
     uh.func(0*h) - ue.func(duration),  
 )
+
 
 #Objective function and gradient
 def obj(free):
@@ -134,9 +135,9 @@ prob = Problem(
     known_trajectory_map=traj_map,
     instance_constraints=instance_constraints,
     bounds=bounds,
-    integration_method='midpoint',
+    integration_method='backward euler',
     time_symbol=time_symbol,
-    parallel=True
+    parallel=True    
 )
 
 #Set an initial guess
@@ -147,7 +148,79 @@ solution, info = prob.solve(initial_guess)
 
 #Pull out solution trajectory
 xs, rs, _, h_val = prob.parse_free(solution)
+
 times = np.linspace(0.0, (num_nodes - 1)*h_val, num=num_nodes)
 if info['status'] in (0, 1):
-    np.savetxt(f'human_gait_{num_nodes}_nodes_solution.csv', solution,
+    np.savetxt(f'stand_{num_nodes}_nodes_solution.csv', solution,
                fmt='%.2f')
+
+def animate(fname='animation.gif'):
+    trunk, pelvis, rthigh, rshank, rfoot, lthigh, lshank, lfoot = segments
+
+    fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
+
+    hip_proj = origin.locatenew('m', qax*ground.x)
+    scene = Scene3D(ground, hip_proj, ax=ax)
+
+    # creates the stick person
+    scene.add_line([
+        rshank.joint,
+        rfoot.toe,
+        rfoot.heel,
+        rshank.joint,
+        rthigh.joint,
+        pelvis.joint,
+        trunk.joint,
+        trunk.mass_center,
+        trunk.joint,
+        pelvis.joint,
+        lthigh.joint,
+        lshank.joint,
+        lfoot.heel,
+        lfoot.toe,
+        lshank.joint,
+    ], color="k")
+
+    # creates a moving ground (many points to deal with matplotlib limitation)
+    scene.add_line([origin.locatenew('gl', s*ground.x)
+                    for s in np.linspace(-2.0, 2.0)],
+                   linestyle='--', color='tab:green', axlim_clip=True)
+
+    # adds CoM and unit vectors for each body segment
+    for seg in segments:
+        scene.add_body(seg.rigid_body)
+
+    # show ground reaction force vectors at the heels and toes, scaled to
+    # visually reasonable length
+    scene.add_vector(contact_force(rfoot.toe, ground, origin)/600.0,
+                     rfoot.toe, color="tab:blue")
+    scene.add_vector(contact_force(rfoot.heel, ground, origin)/600.0,
+                     rfoot.heel, color="tab:blue")
+    scene.add_vector(contact_force(lfoot.toe, ground, origin)/600.0,
+                     lfoot.toe, color="tab:blue")
+    scene.add_vector(contact_force(lfoot.heel, ground, origin)/600.0,
+                     lfoot.heel, color="tab:blue")
+
+    scene.lambdify_system(states + specified + constants)
+    gait_cycle = np.vstack((
+        xs,  # q, u shape(2n, N)
+        np.zeros((3, len(times))),  # Fax, Fay, Ta (hand of god), shape(3, N)
+        rs,  # r, shape(q, N)
+        np.repeat(np.atleast_2d(np.array(list(par_map.values()))).T,
+                  len(times), axis=1),  # p, shape(r, N)
+    ))
+    scene.evaluate_system(*gait_cycle[:, 0])
+
+    scene.axes.set_proj_type("ortho")
+    scene.axes.view_init(90, -90, 0)
+    scene.plot()
+
+    ax.set_xlim((-0.8, 0.8))
+    ax.set_ylim((-0.2, 1.4))
+    ax.set_aspect('equal')
+
+    ani = scene.animate(lambda i: gait_cycle[:, i], frames=len(times),
+                        interval=h_val*1000)
+    ani.save(fname, fps=int(1/h_val))
+
+animation = animate('quiet_stance.gif')
